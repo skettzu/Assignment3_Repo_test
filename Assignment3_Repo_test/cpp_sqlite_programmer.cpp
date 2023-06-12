d6f32c96e5a76f9a94c2e98063073882d61c1d51
Derek Huang <huangd8@wit.edu>
2023-06-12 11:51:16 -0400

    cpp

--- Assignment3_Repo_test/cpp_sqlite_programmer.cpp
+++ Assignment3_Repo_test/cpp_sqlite_programmer.cpp
@@ -30,17 +30,6 @@ int main(int argc, char** argv)
 {
 	sqlite3* DB;
 
-	/*******************************************************************
-	 Creating a table
-	 Create a string then pass the string into the sqlite3_exec function
-	********************************************************************/
-	/*string table = "CREATE TABLE PROGRAMMER("
-		"ID INTEGER PRIMARY KEY, "
-		"NAME TEXT NOT NULL, "
-		"SURNAME TEXT NOT NULL, "
-		"BIRTHYEAR INTEGER NOT NULL); ";
-	*/
-
 	int exit = 0;
 
 	exit = sqlite3_open("assignment3.db", &DB); // open the database
@@ -52,122 +41,63 @@ int main(int argc, char** argv)
 	}
 	char* messageError;
 	string query;
-	/*
-	// execute the create table command
-	// sqlite3_exec( pointer to database file, string for sql command, callback function (used to respond to queries, not used here), input to callback, error message address)
-	exit = sqlite3_exec(DB, table.c_str(), NULL, 0, &messageError);
+	
+	/*Call command course queries the database for the list of class names and corresponding times*/
 
-	if (exit != SQLITE_OK)
-	{
-		std::cerr << "Error Create Table" << std::endl;
-		sqlite3_free(messageError);
-	}
-	else
-		cout << "Table created Successfully" << std::endl;
-
-	/*******************************************************************
-	 Inserting values into a table.
-	 Create a string then pass the string into the sqlite3_exec function
-	*******************************************************************
-	// hard-code (push) a few values into the database - NOTE you can create a single string with multiple INSERT commands
-	string sql("INSERT INTO PROGRAMMER VALUES(1, 'ADA', 'LOVELACE', 1815);"
-		"INSERT INTO PROGRAMMER VALUES(2, 'GRACE', 'HOPPER', 1906);"
-		"INSERT INTO PROGRAMMER VALUES(3, 'MARY KENNETH', 'KELLER', 1913);"
-		"INSERT INTO PROGRAMMER VALUES(4, 'EVELYN', 'BOYD GRANVILLE', 1924);"
-		"INSERT INTO PROGRAMMER VALUES(5, 'CAROL', 'SHAW', 1955);");
-
-	// execute the command
-	exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messageError);
-
-	if (exit != SQLITE_OK)
-	{
-		std::cerr << "Error Insert" << std::endl;
-		sqlite3_free(messageError);
-	}
-	else
-		std::cout << "Records created Successfully!" << std::endl;
-
-	/***********************************************
-	 print all data in the table with SELECT * FROM
-	 create string with query then execute
-	 **********************************************/
-	/*
-	string query = "SELECT * FROM PROGRAMMER;";
-
-	cout << endl
-		<< query << endl; // print the string to screen
-
-	// you need the callback function this time since there could be multiple rows in the table
-	sqlite3_exec(DB, query.c_str(), callback, NULL, NULL);
-	*/
-	/*
-	/**********************************************
-	 adding from a file or user input
-	 get input --> create string --> call command
-	*********************************************
-	string fname, lname;
-	string year; // year is an integer in the database table, but we beed to create a string to pass in to the sql command
-	cout << endl
-		<< "Enter the first name, last name, and birth year of a famous programmer separated by spaces: ";
-	cin >> fname >> lname >> year;
-	cout << endl;
-
-	// Adding from a file or a user input means some string additions (see below)
-	string UID = "6";
-	string userInput("INSERT INTO PROGRAMMER VALUES(" + UID + ",'" + fname + "','" + lname + "'," + year + ");");
-
-	exit = sqlite3_exec(DB, userInput.c_str(), callback, NULL, NULL);
-	*/
-	/***********************************
-	 refining queries --> SELECT example
-	 create string --> call command
-	***********************************/
-	string course = "SELECT Title FROM COURSES WHERE department = 'BSCO';";
+	string course = "SELECT Title, time FROM COURSES;";
 	cout << endl
 		<< "SQL Command: " << course << endl;
-	cout << "The following courses are taught in BSCO" << endl;
-	exit = sqlite3_exec(DB, course.c_str(), callback, NULL, NULL);
+	cout << "The following is the courses and the corresponding times: " << endl;
+	exit = sqlite3_exec(DB, course.c_str(), callback, NULL, NULL);		// calls callback since there could be multiple rows
 	if (exit != SQLITE_OK) {
 		cout << "error";
 	}
+
+	/*Call command course queries the database for courses under the BSCO dept*/
+
+	course = "SELECT Title FROM COURSES WHERE department = 'BSCO';";
+	cout << endl
+		<< "SQL Command: " << course << endl;
+	cout << "The following courses are taught in BSCO" << endl;
+	exit = sqlite3_exec(DB, course.c_str(), callback, NULL, NULL);		// calls callback since there could be multiple rows
+	if (exit != SQLITE_OK) {
+		cout << "error";
+	}
+
+	/*Call command course queries the database for instructors' names that can teach the above courses (linking)*/
 
 	course = "SELECT NAME, SURNAME FROM INSTRUCTOR WHERE DEPT = 'BSCO'; ";
 	cout << endl
 		<< "SQL Command: " << course << endl;
 	cout << "The following instructors can teach those courses: " << endl;
-	exit = sqlite3_exec(DB, course.c_str(), callback, NULL, NULL);
+	exit = sqlite3_exec(DB, course.c_str(), callback, NULL, NULL);		// calls callback since there could be multiple rows
 	if (exit != SQLITE_OK) {
 		cout << "error";
 	}
+
+	/*Call command course queries the database for courses under the BSEE dept*/
 
 	course = "SELECT Title FROM COURSES WHERE department = 'BSEE';";
 	cout << endl
 		<< "SQL Command: " << course << endl;
 	cout << "The following courses are taught in BSEE" << endl;
-	exit = sqlite3_exec(DB, course.c_str(), callback, NULL, NULL);
+	exit = sqlite3_exec(DB, course.c_str(), callback, NULL, NULL);		// calls callback since there could be multiple rows
 	if (exit != SQLITE_OK) {
 		cout << "error";
 	}
+
+	/*Call command course queries the database for instructors' names that can teach the above courses (linking)*/
 
 	course = "SELECT NAME, SURNAME FROM INSTRUCTOR WHERE DEPT = 'BSEE'; ";
 	cout << endl
 		<< "SQL Command: " << course << endl;
 	cout << "The following instructors can teach those courses: " << endl;
-	exit = sqlite3_exec(DB, course.c_str(), callback, NULL, NULL);
+	exit = sqlite3_exec(DB, course.c_str(), callback, NULL, NULL);		// calls callback since there could be multiple rows
 	if (exit != SQLITE_OK) {
 		cout << "error";
 	}
-	/*
-	query = "SELECT * FROM PROGRAMMER;";
+	
 
-	cout << endl
-		<< query << endl; // print the string to screen
-
-	// you need the callback function this time since there could be multiple rows in the table
-	sqlite3_exec(DB, query.c_str(), callback, NULL, NULL);
-	*/
-	// other possible commands from SQL (update, delete, etc.), try those. Same concept, create string then call command
-
-	sqlite3_close(DB);
+	sqlite3_close(DB);		// closes the database
 	return 0;
 }
