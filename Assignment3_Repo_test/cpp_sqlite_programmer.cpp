f4ba61b8e7b4b8223e30cf3fd7e8d9244fdba7dd
Derek Wong <99dwong@gmail.com>
2023-06-12 22:35:47 -0400

    Create Table function

--- Assignment3_Repo_test/cpp_sqlite_programmer.cpp
+++ Assignment3_Repo_test/cpp_sqlite_programmer.cpp
@@ -37,7 +37,7 @@ int main(int argc, char** argv)
 		cout << "error";
 	}
 	else {
-		cout << "open success";
+		cout << "open success" << endl;
 	}
 	char* messageError;
 	string query;
@@ -51,13 +51,102 @@ int main(int argc, char** argv)
 		cout << "4 - Create Table" << endl;
 		cout << "5 - Update" << endl;
 		cout << "6 - Remove" << endl;
+		cout << "0 - Exit" << endl;
+
+		cin >> user_opt;
 
 		if (user_opt == 1) {
 			cout << "Search for: ";
 
 		}
+		// Create table option
+		else if (user_opt == 4) {
+			string title;
+			string table;
+			string sql_query;
+			int attr = 0;
+			int usr_attr;
+			// Inquires User how many attributes they want their table to have
+			cout << "How many attributes does your table have?" << endl;
+			cin >> usr_attr;
+			cout << "What is the title of your table?" << endl;
+			cin >> title;
+			table = "CREATE TABLE " + title + "(";
+			// Asks users to input attributes with corresponding inputs until attribute size is reached
+			while (attr < usr_attr) {
+				// temp and temp_d are temporary variables storing user inputs
+				string temp;
+				string temp_d;
+				cout << "Enter an attribute: " << endl;
+				// On first iteration clear the pending buffer from previous cins
+				if (attr == 0) {
+					cin.ignore();
+				}
+				getline(cin, temp);
+				// starts building sql command starting with attribute name
+				sql_query = temp;
+				temp = "";
+				cout << "Enter data type(INTEGER,TEXT): " << endl;
+				getline(cin, temp);
+				sql_query = sql_query + " " + temp;
+				cout << "Primary Key?(Y/N): " << endl;
+				getline(cin, temp_d);
+				// asks if is primary key
+				if (temp_d == "Y") {
+					sql_query = sql_query + " PRIMARY KEY,";
+				}
+				else if (temp_d == "N") {
+					cout << "NULL or NOT NULL? " << endl;
+					getline(cin, temp);
+					// checks if it is last attribute to terminate the table string with right bracket
+					if (attr == usr_attr - 1) {
+						sql_query = sql_query + " " + temp + "); ";
+					}
+					else {
+						sql_query = sql_query + " " + temp + ", ";
+					}
+				}				
+				attr++;
+				// clear temp variables
+				temp = "";
+				temp_d = "";
+				// add current sql attribute to table 
+				table = table + sql_query;
+			}
+			// prints final table string
+			cout << table << endl;
+			exit = sqlite3_exec(DB, table.c_str(), NULL, 0, &messageError);	// calls callback since there could be multiple rows
+
+			if (exit != SQLITE_OK)
+			{
+				std::cerr << "Error Create Table" << std::endl;
+				sqlite3_free(messageError);
+			}
+			else {
+				cout << "Table created Successfully" << std::endl;
+			}
+			/*
+			for (int i = 0; i < attr_num; i++) {
+				string temp_element;
+				cout << "Enter the number " << i + 1 << "element" << endl;
+				cin >> temp_element;
+				attr[i] = temp_element;
+			}
+			*/
+
+		}
+		else if (user_opt == 5) {
+
+		}
+		else if (user_opt == 6) {
+
+		}
+		else {
+			cout << "Invalid Choice!" << endl;
+		}
 	}
-	/*Call command course queries the database for the list of class names and corresponding times*/
+	/*
+	/*Call command course queries the database for the list of class names and corresponding times
 
 	string course = "SELECT Title, time FROM COURSES;";
 	cout << endl
@@ -68,7 +157,7 @@ int main(int argc, char** argv)
 		cout << "error";
 	}
 
-	/*Call command course queries the database for courses under the BSCO dept*/
+	/*Call command course queries the database for courses under the BSCO dept
 
 	course = "SELECT Title FROM COURSES WHERE department = 'BSCO';";
 	cout << endl
@@ -79,7 +168,7 @@ int main(int argc, char** argv)
 		cout << "error";
 	}
 
-	/*Call command course queries the database for instructors' names that can teach the above courses (linking)*/
+	/*Call command course queries the database for instructors' names that can teach the above courses (linking)
 
 	course = "SELECT NAME, SURNAME FROM INSTRUCTOR WHERE DEPT = 'BSCO'; ";
 	cout << endl
@@ -90,7 +179,7 @@ int main(int argc, char** argv)
 		cout << "error";
 	}
 
-	/*Call command course queries the database for courses under the BSEE dept*/
+	/*Call command course queries the database for courses under the BSEE dept
 
 	course = "SELECT Title FROM COURSES WHERE department = 'BSEE';";
 	cout << endl
@@ -101,7 +190,7 @@ int main(int argc, char** argv)
 		cout << "error";
 	}
 
-	/*Call command course queries the database for instructors' names that can teach the above courses (linking)*/
+	/*Call command course queries the database for instructors' names that can teach the above courses (linking)
 
 	course = "SELECT NAME, SURNAME FROM INSTRUCTOR WHERE DEPT = 'BSEE'; ";
 	cout << endl
@@ -111,7 +200,7 @@ int main(int argc, char** argv)
 	if (exit != SQLITE_OK) {
 		cout << "error";
 	}
-	
+	*/
 
 	sqlite3_close(DB);		// closes the database
 	return 0;
